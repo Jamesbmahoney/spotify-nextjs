20d51edba3e6bfbf97f90da732a28f8fe0169ded
James Barry <jbm03003@gmail.com>
2022-08-09 16:19:06 -0400

    added scrollbar functionality

---
+++ components/Center.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Center() {
+  return (
+    <div className="flex flex-grow text-white">
+      <h1>I Am Center</h1>
+    </div>
+  );
+}
+
+export default Center;
