5c29c8f740680b4ed53594a6644c8bf716ceb68f
James Barry <jbm03003@gmail.com>
2022-08-09 17:27:39 -0400

    added hooks to add playlist to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   HomeIcon,
   SearchIcon,
@@ -8,9 +8,20 @@ import {
   PlusCircleIcon,
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
+import useSpotify from "../hooks/useSpotify";
 
 function Sidebar() {
+  const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
 
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen ">
@@ -49,15 +60,11 @@ function Sidebar() {
         <hr className="border-t-[0.1px] border-gray-900" />
 
         {/* Playlists */}
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
-        <p className="cursor-pointer hover:text-white">Playlist name....</p>
+        {playlists.map((playlist) => (
+          <p key={playlist.id} className="cursor-pointer hover:text-white">{playlist.name}</p>
+        ))};
+        
+       
       </div>
     </div>
   );
