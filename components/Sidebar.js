5de16ebcfd1969c5934307c5baa399ae025ef0d5
James Barry <jbm03003@gmail.com>
2022-08-11 14:03:30 -0400

    cleaned up some icons in sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,10 +3,10 @@ import {
   HomeIcon,
   SearchIcon,
   LibraryIcon,
-  HeartIcon,
   RssIcon,
   PlusCircleIcon,
 } from "@heroicons/react/outline";
+import { HeartIcon } from "@heroicons/react/solid";
 import { useRecoilState } from "recoil";
 import { signOut, useSession } from "next-auth/react";
 import useSpotify from "../hooks/useSpotify";
@@ -49,11 +49,11 @@ function Sidebar() {
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
+          <HeartIcon className="h-5 w-5  text-blue-500" />
           <p>Liked Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5   text-green-500" />
           <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
